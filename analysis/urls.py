f768e4594a677250da7ac34f0b256143455673d9
Ritika <ritika140601@gmail.com>
2021-10-01 17:45:59 +0530

    modules

--- analysis/urls.py
+++ analysis/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('ambulance/',views.ambulance, name='ambulance'),
+    path('icubeds/',views.icubeds, name ='icubeds'),
+    path('beds/',views.beds, name='beds'),
+    path('oxygen/',views.oxygen, name='oxygen'),
+    path('plasma/',views.plasma, name='plasma'),
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -8,3 +8,16 @@ def home(request):
 
 def ambulance(request):
     return render(request, 'ambulance.html')
+
+def beds(request):
+    return render(request, 'beds.html')
+
+def icubeds(request):
+    return render(request, 'icubeds.html')
+
+def oxygen(request):
+    return render(request, 'oxygen.html')
+    
+def plasma(request):
+    return render(request, 'plasma.html')
+
