32b098fbccc8589ae9427a032d966d766066e9f1
Ritika <ritika140601@gmail.com>
2021-10-01 17:12:09 +0530

    static

--- analysis/views.py
+++ analysis/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
     return render(request, 'stats.html')
-    
+
 def ambulance(request):
     return render(request, 'ambulance.html')
